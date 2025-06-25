82a7dafe6625c0e26fa97ecfc8f774b93bf44401
Christian Povis <christian.povis@gmail.com>
2025-06-25 11:55:23 -0500

    Se modifico: index.html -> se paso a dashboard.html index.html ahora contorla el logueo con cognito

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-    base: '/',
+    base: './',
     build: {
         outDir: 'dist',
         emptyOutDir: true,
         assetsDir: 'assets',
         rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                dashboard: resolve(__dirname, 'dashboard.html'),
+            },
             output: {
                 entryFileNames: 'assets/[name].[hash].js',
                 chunkFileNames: 'assets/[name].[hash].js',
